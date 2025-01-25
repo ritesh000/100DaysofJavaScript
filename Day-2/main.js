1fe96bfa203eca48f7a04c99f2a826cee440157d
ritesh malav <riteshmalavo@gmail.com>
2025-01-25 05:39:58 +0000

    Day-2

---
+++ Day-2/main.js
@@ -0,0 +1,20 @@
+// variables
+var name="ritesh malav";
+
+console.log(name);
+// change value
+name ='abe';
+
+console.log(name);
+
+//role of variable
+//  first_name  vaild
+//  first$name  vaild
+//  first1name  vaild
+//  firstname$  vaild
+//  firstname_   vaild
+//  _firstname    vaild
+
+
+ //first name  not vaild
+
