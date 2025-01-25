362f4ad07e8658d6726d0f141d2e552d9d950018
ritesh malav <riteshmalavo@gmail.com>
2025-01-25 06:07:11 +0000

    DAY-4

---
+++ Day-4/main.js
@@ -0,0 +1,44 @@
+let name ="ritesh"
+let age = 21;
+let isvalue = true;
+
+console.log( typeof name);
+console.log( typeof age);
+console.log( typeof isvalue);
+//  
+let person="ram";
+console.log( typeof person);
+person=5;
+console.log( typeof person);
+
+let age1 = 21 + "";
+console.log( typeof age1);
+
+let num1 = + "51";
+console.log( typeof num1);
+
+let myage = 18;
+console.log(typeof myage);
+myage =  String(myage);
+console.log(typeof myage);
+
+let hey ="51";
+console.log(typeof hey);
+hey= Number(hey);
+console.log(typeof hey);
+
+// String concatenation
+let firstName="ritesh";
+let lastName="malav";
+console.log(firstName+" "+lastName);
+
+let num01="12";
+let num02="13";
+
+let k=num01+" "+num02;
+console.log(k);
+console.log(typeof k);
+
+let sum=+num01+ + +num02;
+console.log(sum);
+console.log(typeof sum);
