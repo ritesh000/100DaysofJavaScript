7aaa088e5c0e31da9072d38e66b9afecb706fc7a
ritesh malav <riteshmalavo@gmail.com>
2025-01-25 06:30:59 +0000

    Day-7

---
+++ Day-7/main.js
@@ -0,0 +1,19 @@
+// if else and if else if 
+let age = 5;
+if (age > 18) {
+    console.log("young man");
+} else {
+    console.log("child ")
+}
+
+let age1 = 8
+if (age > 21) {
+    console.log("class 12 student");
+} else if (age < 21 && age > 10) {
+    console.log("class 10 student");
+} else {
+    console.log("class 6-9 student");
+}
+
+
+// ternary operator
