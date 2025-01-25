3c8a8be4d59a6215138a181cb40975dba2538e06
ritesh malav <riteshmalavo@gmail.com>
2025-01-25 05:52:52 +0000

    Day-3

---
+++ Day-3/main.js
@@ -0,0 +1,10 @@
+// string
+
+let name = " ritesh malav ";
+console.log(name);
+
+console.log(name.length);
+console.log(name.trim());
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.trim().slice(0,4));
