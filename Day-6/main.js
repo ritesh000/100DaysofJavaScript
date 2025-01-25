1a70fd02642c2901357fbf6199a747818b00bf5e
ritesh malav <riteshmalavo@gmail.com>
2025-01-25 06:21:24 +0000

    Day-6

---
+++ Day-6/main.js
@@ -0,0 +1,25 @@
+// boolean and comparison operator
+
+// boolean return true false
+
+let num1 =7;
+let num2 =7;
+ console.log(num1>num2);
+ console.log(num1==num2);
+ console.log(num1===num2);
+ console.log(num1!==num2);
+
+ let num3 =7;
+let num4 ="7";
+ console.log(num3>num4);
+ console.log(num3==num4);
+ console.log(num3===num4);
+
+ let num5 =7;
+ let num6 =8;
+  console.log(num5<num6);
+  console.log(num5!=num6);
+  console.log(num5===num6);
+
+
+  // truthy abd falsy condition
